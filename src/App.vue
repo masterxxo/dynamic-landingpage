0371f0f2279ffd55aadd91732812db3d8b529e39
Mateusz Róg <kontakt@matrog.pl>
2026-07-21 16:47:26 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import HeroSection from './components/HeroSection.vue';
+</script>
+
+<template>
+  <main class="relative min-h-screen w-screen overflow-x-hidden">
+    <HeroSection />
+  </main>
+</template>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative h-dvh w-screen overflow-x-hidden">
+    <div id="video-frame" class="relative z-10 h-dvh w-creen overflow-hidden rounded-lg bg-blue-75">
+      <div>
+        <div class="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer rounded-lg overflow-hidden">
+          MiniVideoPlayer
+        </div>
+      </div>
+    </div>
+    <!-- <h1 class="text-5xl text-violet-300 font-bold font-circular-web">Welcome to Awwwards</h1> -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, type Ref } from 'vue';
+const currentIndex: Ref<number> = ref(1);
+const hasClicked: Ref<boolean> = ref(false);
+const isLoading: Ref<boolean> = ref(true);
+const loadedVideos: Ref<number> = ref(0);
+
+const handleMiniVideoClick = () => {
+
+}
+</script>
